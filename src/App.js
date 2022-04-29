f78a8ae4da746c79a03563444e600b61790c0bb5
PrincessMaggy <edoziemagdalene@gmail.com>
2022-04-29 17:20:08 +0100

    split up components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react";
+import Pad from "./components/Pad";
 
 const clips = [{
   "keyTrigger": "Q",
@@ -72,43 +72,5 @@ const App = () => {
     </div>
   );
 }
-
-const Pad = ({clip}) =>{
-
-useEffect(()=> {
-
-  const [active, setActive] = useState(false)
-
-   document.addEventListener('keydown', handleKeyPress)
-   return () =>{
-     document.removeEventListener('keydown',handleKeyPress)
-   }
-})
-
-
-const handleKeyPress =(e) =>{
-
-  if( e.keyCode === clip.keyCode){
-    playSound();
-  }
-
-}
-
-const playSound = () =>{
-  const audioTape = document.getElementById(clip.keyTrigger);
-  setActive(true);
-  setTimeout(()=> setActive(false), 200);
-  audioTape.currentTime =0;
-  audioTape.play();
-}
-
-  return (
-    <button onClick={playSound} className={`btn btn-secondary p-4 m-3 ${active && "btn-warning"}`}>
-    <audio className="clip" id={clip.keyTrigger} src={clip.url} />
-    {clip.keyTrigger} </button>
-  
-  )
-}
-
 
 export default App;
---
+++ src/components/Pad.js
@@ -0,0 +1,39 @@
+import React,{useEffect, useState} from "react";
+
+const Pad = ({clip}) =>{
+    const [active, setActive] = useState(false)
+  
+  useEffect(()=> {
+     document.addEventListener('keydown', handleKeyPress)
+     return () =>{
+       document.removeEventListener('keydown',handleKeyPress)
+     }
+  })
+  
+  
+  const handleKeyPress =(e) =>{
+  
+    if( e.keyCode === clip.keyCode){
+      playSound();
+    }
+  
+  }
+  
+  const playSound = () =>{
+    const audioTape = document.getElementById(clip.keyTrigger);
+    setActive(true);
+    setTimeout(()=> setActive(false), 200);
+    audioTape.currentTime =0;
+    audioTape.play();
+  }
+  
+    return (
+      <button onClick={playSound} className={`btn btn-secondary p-4 m-3 ${active && "btn-warning"}`}>
+      <audio className="clip" id={clip.keyTrigger} src={clip.url} />
+      {clip.keyTrigger} </button>
+    
+    )
+  }
+  
+  
+export default Pad;  
